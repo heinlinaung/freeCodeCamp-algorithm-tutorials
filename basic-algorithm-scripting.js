bb6035bf3b63724f3e8eeddeb1a71c54e2bf61b9
Hein Lin Aung <heinlinaung@amdon.com>
2017-09-19 11:34:31 +0630

    Finished : Mutations algorithm

--- basic-algorithm-scripting.js
+++ basic-algorithm-scripting.js
@@ -127,3 +127,13 @@ function slasher(arr, howMany) {
 slasher([1, 2, 3], 3);
 // === END ====
 
+//Mutations
+function mutation(arr) {
+  let arr1 = arr[0].toLowerCase().split('');
+  let arr2 = arr[1].toLowerCase().split('');
+  return arr2.every(function(el) {
+    return !(arr1.indexOf(el)<0);
+  })
+}
+mutation(["hello", "hey"]);
+// === END ====
