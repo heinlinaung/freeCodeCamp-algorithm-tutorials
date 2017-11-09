57d785341e8a53c9e1b6aed6a4cbcd63bb031982
Hein Lin Aung <heinlinaung@amdon.com>
2017-11-09 15:57:18 +0630

    [DONE] : Caesars Cipher

--- basic-algorithm-scripting.js
+++ basic-algorithm-scripting.js
@@ -169,4 +169,21 @@ function getIndexToIns(arr, num) {
   return [...arr, num].sort((a,b)=> { return a - b }).indexOf(num)
 }
 getIndexToIns([40, 60], 50);
+
+// === END ====
+
+//Caesars Cipher
+function rot13(str) { // LBH QVQ VG!
+  return [...str].map(s=>{
+    let x = s.charCodeAt();
+    if(x < 65 || x > 90){
+      return s;
+    }else if(x < 78){
+      return String.fromCharCode(x+13);
+    }
+    return String.fromCharCode(x-13);
+  }).join('')
+
+}
+rot13("SERR PBQR PNZC");
 // === END ====
