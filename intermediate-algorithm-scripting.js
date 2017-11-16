8b2eb8605d65ed03ed22516975af1204263c7200
Hein Lin Aung <heinlinaung@amdon.com>
2017-11-16 10:37:34 +0630

    [DONE] : Diff Two Arrays

--- intermediate-algorithm-scripting.js
+++ intermediate-algorithm-scripting.js
@@ -13,4 +13,17 @@ function sumAll(arr) {
 sumAll([1, 4]);
 // === END ====
 
+//Diff Two Arrays
+function diffArray(arr1, arr2) {
+  return arr1.filter((ele,idx)=>{
+    if(arr2.indexOf(ele) < 0){
+      return ele;
+    }else{
+      arr2.splice(arr2.indexOf(ele),1)
+      return false;
+    }
+  }).concat(arr2)
+}
 
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+// === END ====
