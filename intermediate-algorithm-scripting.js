369d0fbe25b76abf2ad0591d6f0ffa876d677c8d
Hein Lin Aung <heinlinaung@amdon.com>
2017-11-16 16:33:08 +0630

    [DONE] : Wherefore art thou

--- intermediate-algorithm-scripting.js
+++ intermediate-algorithm-scripting.js
@@ -48,3 +48,16 @@ var convertToRoman = function(num) {
 
 convertToRoman(36);
 // === END ====
+
+//Wherefore art thou
+function whatIsInAName(collection, source) {
+  var srcKeys = Object.keys(source);
+
+  return collection.filter(function (obj) {
+    return srcKeys.every(function (key) {
+      return obj.hasOwnProperty(key) && obj[key] === source[key];
+    });
+  });
+}
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" })
+// === END ====
