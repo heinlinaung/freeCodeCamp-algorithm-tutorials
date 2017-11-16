56aa956111bb67facc141ef95104583eafc800ce
Hein Lin Aung <heinlinaung@amdon.com>
2017-11-16 15:36:24 +0630

    [DONE] : Roman Numeral Converter

--- intermediate-algorithm-scripting.js
+++ intermediate-algorithm-scripting.js
@@ -27,3 +27,24 @@ function diffArray(arr1, arr2) {
 
 diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
 // === END ====
+
+//Roman Numeral Converter
+var convertToRoman = function(num) {
+
+  var decimalValue = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ];
+  var romanNumeral = [ 'M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I' ];
+
+  var romanized = '';
+
+  for (var index = 0; index < decimalValue.length; index++) {
+    while (decimalValue[index] <= num) {
+      romanized += romanNumeral[index];
+      num -= decimalValue[index];
+    }
+  }
+
+  return romanized;
+}
+
+convertToRoman(36);
+// === END ====
