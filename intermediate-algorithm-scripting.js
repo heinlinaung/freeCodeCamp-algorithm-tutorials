4d9eedac2ee16c1e3d93e6080c1932c86f268579
Hein Lin Aung <heinlinaung@amdon.com>
2017-11-19 19:03:17 +0630

    [DONE] : Spinal Tap Case

--- intermediate-algorithm-scripting.js
+++ intermediate-algorithm-scripting.js
@@ -138,3 +138,12 @@ function convertHTML(str) {
 
 convertHTML("Dolce & Gabbana");
 // === END ====
+
+//Spinal Tap Case
+function spinalCase(str) {
+  var regex = /\s+|_+/g;
+  str = str.replace(/([a-z])([A-Z])/g, '$1 $2');
+  return str.replace(regex, '-').toLowerCase();
+}
+spinalCase('This Is Spinal Tap');
+// === END ====
